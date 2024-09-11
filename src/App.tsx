3033205e781e47c338893929be8b6f74f3e13909
Andres-debug <andresf.diazm@unac.edu.co>
2024-09-10 20:29:59 -0500

    Characters Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <>
-    {/* <CharacterList/> */}
-     <CharacterFilter/>
+    <CharacterList/>
+     {/* <CharacterFilter/> */}
     </>
   )
 }
--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -6,23 +6,42 @@ interface Character{
     image:string
 }
 
+interface Info {
+    next: string | null,
+    prev: string | null,
+}
+
  export const CharacterList: React.FC = ()=>{
 
     const [characters,setCharacters]= useState<Character[]>([]);
-
     const [isLoading,setIsLoading]= useState<boolean>(true);
+    const [info,setInfo]= useState<Info>({next:null,prev:null});
+    const [page,setPage]=useState<number>(1)
 
     useEffect(()=>{
-        fetch('https://rickandmortyapi.com/api/character')
+        fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
         .then(response=>response.json())
         .then(data=> {
             setCharacters(data.results)
+            setInfo(data.info)
             setIsLoading(false);
         }).catch(error=>{
             console.error('Error al cargar los personajes: ',error)
             setIsLoading(false)
         })
-    },[])
+    },[page])
+
+    const handlePrev = () =>{
+        if (info.prev){
+            setPage(page-1);
+        }
+    }
+
+    const handleNext = () =>{
+        if(info.next){
+            setPage(page+1)
+        }
+    }
 
     if(isLoading){
         return <p>Cargando personajes...</p>
@@ -30,8 +49,8 @@ interface Character{
 
     return(
         <>
-        <div>
-            <h2>Personajes de Rick and Morty</h2>
+        <div className='character-pagination'>
+            <h2>Personajes de Rick and Morty - Page {page}</h2>
             <ul>
                 {
                     characters.map(character=>(
@@ -43,6 +62,8 @@ interface Character{
                 }
             </ul>
             </div>
+            <button onClick={handlePrev} disabled={!info.prev}>Anterior</button>
+            <button onClick={handleNext} disabled={!info.next}>Siguiente</button>
         </>
     )
 }
